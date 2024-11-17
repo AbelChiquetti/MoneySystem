b651602611cd56cceac8641b969be1fdd4ea4c69
AbelChiquetti <abelcpl86@gmail.com>
2024-11-17 14:58:44 -0300

    Add hora na impressão em OS e Vendas. Correções e melhorias em vários arquivos

--- application/views/tema/rodape.php
+++ application/views/tema/rodape.php
@@ -1,29 +1,31 @@
 <div class="row-fluid">
     <div id="footer" class="span12">
-        <a class="pecolor" href="https://github.com/RamonSilva20/mapos" target="_blank">
-            <?= date('Y') ?> &copy; Ramon Silva - MoneySystem/MapOS - Versão: <?= $this->config->item('app_version') ?>
+        <a class="pecolor" href="https://www.instagram.com/moneysystem.br" target="_blank">
+            <?= date('Y') ?> &copy; MoneySystem - Versão: <?= $this->config->item('app_version') ?>
         </a>
     </div>
 </div>
 <!--end-Footer-part-->
 <script src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
 <script src="<?= base_url() ?>assets/js/matrix.js"></script>
-</body>
 <script type="text/javascript">
     $(document).ready(function() {
-        var dataTableEnabled = '<?= $configuration['control_datatable'] ?>';
-        if(dataTableEnabled == '1') {
-            $('#tabela').dataTable( {
-                "ordering": false,
-                "info": false,
-                "language": {
-                    "url": "<?= base_url() ?>assets/js/dataTable_pt-br.json",
-                },
-                "oLanguage": {
-                    "sSearch": "Pesquisa rápida na tabela abaixo:"
-                }
-            } );
-        }
-    } );
+        var table = $('#tabela').DataTable({
+            info: false,
+            language: {
+                url: "<?= base_url() ?>assets/js/dataTable_pt-br.json"
+            },
+            pageLength: 6
+        });
+        $('#customSearch').on('keyup', function() {
+            table.search(this.value).draw();
+        });
+    });
 </script>
+<style>
+    #tabela_filter, #tabela_length {
+        display: none;
+    }
+</style>
+
 </html>
